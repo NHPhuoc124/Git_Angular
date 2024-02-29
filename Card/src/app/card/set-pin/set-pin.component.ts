fb734c2dbf7837c0ed3d13b8e67b3fad26d49d0e
NHPhuoc <nhp124@gmail.com>
2024-02-29 07:58:30 +0700

    add Card - View - Set-Pin

---
+++ Card/src/app/card/set-pin/set-pin.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-set-pin',
+  standalone: true,
+  imports: [],
+  templateUrl: './set-pin.component.html',
+  styleUrl: './set-pin.component.css'
+})
+export class SetPinComponent {
+
+}
